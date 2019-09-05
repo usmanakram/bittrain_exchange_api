58c0b3907162dde63d9c68fa096a3b27eab4eec0
Usman Akram <usman.akram99@hotmail.com>
2019-09-05 12:47:10 +0500

    curl testing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -203,7 +203,7 @@ class UsersController extends Controller
 		var_dump($validatedData);
 
 		// $response = $validatedData;
-		$response = $this->authFromBittrain($validatedData);
+		$response = (string)$this->authFromBittrain($validatedData);
 		echo '<br /><br />';
 		echo $response;
 		echo '<pre>';
