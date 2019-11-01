3c1df210f5599736f78ff46b67a78d31d02e9712
Usman Akram <usman.akram99@hotmail.com>
2019-11-01 18:41:17 +0500

    create user balance if doesnot exist

--- app/Http/Controllers/BalancesController.php
+++ app/Http/Controllers/BalancesController.php
@@ -16,9 +16,19 @@ class BalancesController extends Controller
 			}])
 			->get(['id', 'name', 'symbol']);
 
-		$balances->map(function($item) {
-			$item['total_balance'] = $item['balances'][0]['total_balance'] ?? 0;
-			$item['in_order_balance'] = $item['balances'][0]['in_order_balance'] ?? 0;
+		$balances->map(function($item) use ($user_id) {
+			// if balance does not exist for this currency
+			if ( !isset($item['balances'][0]) ) {
+				$item->balances()->create(['user_id' => $user_id, 'in_order_balance' => 0, 'total_balance' => 0]);
+
+				$item['total_balance'] = $item['in_order_balance'] = 0;
+			} else {
+				$item['total_balance'] = $item['balances'][0]['total_balance'];
+				$item['in_order_balance'] = $item['balances'][0]['in_order_balance'];
+			}
+			
+			// $item['total_balance'] = $item['balances'][0]['total_balance'] ?? 0;
+			// $item['in_order_balance'] = $item['balances'][0]['in_order_balance'] ?? 0;
 			unset($item['balances']);
 			return $item;
 		})
