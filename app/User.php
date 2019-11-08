89e8b62d77c81e99eae1dc220c17e001e743b850
Usman Akram <usman.akram99@hotmail.com>
2019-11-08 15:59:35 +0500

    minor bug fixed

--- app/User.php
+++ app/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
 
     public function transactions()
     {
-        return $this->hasMany(Transactions::class);
+        return $this->hasMany(Transaction::class);
     }
 
     public function balances()
